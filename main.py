ca472d9076283c8ed20f7277d59fc7e6afe14988
Narnnam Kongsappaisal <narnnamk@gmail.com>
2026-01-03 16:12:09 -0600

    Move welcome messages to output.py

--- main.py
+++ main.py
@@ -2,19 +2,10 @@ from data import *
 from indicators import *
 from plots import *
 from analysis import *
+from output import *
+from input import *
 
-
-print("-" * 67)
-print("Welcome to Stock Analyzer!")
-print("-" * 67)
-print("This program provides a comprehensive analysis of a stock.")
-print("You can explore the stock's volatility, volume, and moving averages,")
-print("view visual graphs, and receive a summary recommendation at the end.")
-print(
-    "Note: 1mo = 21 trading days, 3mo = 63 trading days, 6mo = 126 trading days, and 1y = 252 trading days."
-)
-print("-" * 67)
-print("-" * 67)
+print_welcome()
 
 ticker = str(input("Enter the stock ticker: ").strip().replace(" ", "").upper())
 loop_ticker_input = True
---
+++ output.py
@@ -0,0 +1,12 @@
+def print_welcome():
+    print("-" * 67)
+    print("Welcome to Stock Analyzer!")
+    print("-" * 67)
+    print("This program provides a comprehensive analysis of a stock.")
+    print("You can explore the stock's volatility, volume, and moving averages,")
+    print("view visual graphs, and receive a summary recommendation at the end.")
+    print(
+        "Note: 1mo = 21 trading days, 3mo = 63 trading days, 6mo = 126 trading days, and 1y = 252 trading days."
+    )
+    print("-" * 67)
+    print("-" * 67)
