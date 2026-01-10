566094af9e06062d85accdf1a71841388242399a
Narnnam Kongsappaisal <narnnamk@gmail.com>
2026-01-10 15:52:03 -0600

    Add plot_volumes function

--- plots.py
+++ plots.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from output import shorten_number
 
 
 def get_x_ticks(days, dates):
@@ -24,15 +25,13 @@ def get_x_ticks(days, dates):
 # Price + Moving Averages (line chart)
 # Use: close_prices (series), fifty_MAs_list, two_hundred_MAs_list
 # Optional extras: horizontal lines for period_high and period_low
-
-
 def plot_price_MAs(closes, days, dates, fifty_MAs, twoH_MAs):
     closes = closes.tail(days)
-    indices, x_ticks = get_x_ticks(days, dates)
     plt.figure(figsize=(12, 6))
     plt.title("Close Price with 50/200-Day MA")
     plt.xlabel("Date (YYYY-MM-DD)")
     plt.ylabel("Close Price (USD)")
+    indices, x_ticks = get_x_ticks(days, dates)
     plt.xticks(indices, x_ticks)
     plt.plot(dates, closes, label="Close Prices")
     plt.plot(dates, fifty_MAs, label="50-Day MA")
@@ -44,6 +43,56 @@ def plot_price_MAs(closes, days, dates, fifty_MAs, twoH_MAs):
 # Use: volumes (series), avg_volume (single number)
 # Plot volume as bars.
 # Plot a flat line at avg_volume across the whole period.
+def plot_volumes(volumes, days, curr_vol, avg_vol, dates):
+    volumes = list(volumes.tail(days))
+    peak_vol = max(volumes)
+    stats_text = f"{'Avg Volume':<12}: {shorten_number(avg_vol):>5}\n{'Peak Volume':<12}: {shorten_number(peak_vol):>5}\n{'Current':<12}: {shorten_number(curr_vol):>5}"
+    avg_vol = float(shorten_number(avg_vol)[:-1])
+    abbrv_volumes = []  # abbreviated volumes
+    for volume in volumes:
+        abbrv_volumes.append(
+            shorten_number(volume)
+        )  # add abbrv form of volume e.g. 5.34M
+
+    suffix = abbrv_volumes[0][-1]
+    if suffix == "K":
+        full_sf = "Thousand"
+    elif suffix == "M":
+        full_sf = "Million"
+    elif suffix == "B":
+        full_sf = "Billion"
+    elif suffix == "T":
+        full_sf = "Trillion"
+    else:
+        full_sf = "Quadrillion"
+
+    no_suffix_abbrv_volumes = []
+    for abbr_v in abbrv_volumes:
+        no_suffix_abbrv_volumes.append(
+            float(abbr_v[: len(abbr_v) - 1])
+        )  # remove suffux and turn into float
+
+    volumes = no_suffix_abbrv_volumes
+    plt.figure(figsize=(12, 6))
+    plt.title("Volume")
+    plt.xlabel("Date (YYYY-MM-DD)")
+    plt.ylabel(f"Volume in {full_sf}")
+    plt.text(
+        0.99,
+        0.98,
+        stats_text,
+        transform=plt.gca().transAxes,
+        fontsize=9,
+        fontfamily="monospace",
+        verticalalignment="top",
+        horizontalalignment="right",
+        bbox=dict(boxstyle="round", facecolor="lightblue", alpha=0.7),
+    )
+    indices, x_ticks = get_x_ticks(days, dates)
+    plt.xticks(indices, x_ticks)
+    plt.bar(dates, volumes)
+    plt.axhline(y=avg_vol, linestyle="--", linewidth=1)
+
 
 # OBV line (good confirmation chart)
 # Use: OBVs_list
